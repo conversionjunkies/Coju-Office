59d5ff7a6322fe7dcc57d2881a99b9e6e25755af
CojuAHeinze <130540769+CojuAHeinze@users.noreply.github.com>
2025-02-05 11:06:35 +0000

    Deploying to gh-pages from master @ 998376a5a019e532a758326c73093adfe2c69f88 🚀

--- assets/main-c977fde9.js
+++
@@ -1,2 +0,0 @@
-console.log("Script started successfully");WA.onInit().then(()=>{console.log("Scripting API ready"),console.log("Player tags: ",WA.player.tags)}).catch(o=>console.error(o));
-//# sourceMappingURL=main-c977fde9.js.map
